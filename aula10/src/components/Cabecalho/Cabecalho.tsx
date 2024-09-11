579d85102c522512d82b31c2d73cf6b7d968d210
Andenik <andenik@yahoo.com.br>
2024-09-10 23:00:59 -0300

    aprendendo scss

--- aula10/src/components/Cabecalho/Cabecalho.tsx
+++ aula10/src/components/Cabecalho/Cabecalho.tsx
@@ -1,11 +1,21 @@
-import { Header } from "../../style/styled";
+// import { Header } from "../../style/styled";
 import Menu from "../Menu/Menu";
+import "../../style/style.scss";
+import { useState } from "react";
 
-export default function Cabecalho(){
-  return(
-    <Header>
-      <h1>Cabeçalho</h1>
-      <Menu/>
-    </Header>
+export default function Cabecalho() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const trocaTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+  return (
+    <div className={`app ${theme}`}>
+      <header className="app-header">
+        <h1>Cabeçalho</h1>
+        <button onClick={() => trocaTheme()}>Mude o Thema</button>
+        <Menu />
+      </header>
+    </div>
   );
 }
